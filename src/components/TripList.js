4282d00bc7fb8c16158fa6bbc25b74a99480d4c1
Nwife Amaeze <ifeeamaeze@gmail.com>
2021-11-25 19:14:24 +0100

    added a pending state, for loading message

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -5,11 +5,12 @@ import { useFetch } from '../hooks/useFetch';
 
 export default function TripList() {
     const [url, setUrl] = useState('http://localhost:8000/trips');
-    const { data: trips } = useFetch(url);
+    const { data: trips, isPending } = useFetch(url);
 
     return (
         <div className="trip-list">
             <h2>Trip List</h2>
+            {isPending && <div>Loading trips...</div>}
             <ul>
                 {trips && trips.map(trip => (
                     <li key={trip.id}>
